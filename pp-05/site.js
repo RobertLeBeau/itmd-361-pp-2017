1764318118706c81e2bf13c7f4d8cffbac75bfa4
Your Name <you@example.com>
2017-11-12 14:29:42 -0600

    Began making chances to js and added a tags to html header

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,4 +1,21 @@
-function ringDoorbell() {
+$.noConflict();
+
+(function($) {
+
   var doorbell = new Audio('media/doorbell.mp3');
+  var image = new Image();
+
+  image.src= 'media/doorbell.png';
+
+  $('html').removeClass('nojs').addClass('js');
+
+$('#dbell').on('click', function() {
   doorbell.play();
-}
+}});
+
+$('h1').append(image);
+$('audio').remove();
+
+
+
+})(jQuery);
